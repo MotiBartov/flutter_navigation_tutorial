d00990584ee1363b27a674acc86f6adb0f39dd6d
Moti Bartov <motib@tikalk.com>
2020-04-18 16:31:25 +0300

    Navigating with Sailor

---
+++ lib/pages/third_page.dart
@@ -0,0 +1,39 @@
+import 'package:flutter/material.dart';
+import 'package:sailor/sailor.dart';
+
+class ThirdPage extends StatelessWidget {
+  final ThirdPageArguments arguments;
+
+  const ThirdPage({Key key, this.arguments}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: Text('Second Page')),
+      body: Container(
+        width: double.infinity,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Text(
+              "Name: ${this.arguments.name}",
+              textAlign: TextAlign.center,
+            ),
+            Text(
+              "Age: ${this.arguments.price}",
+              textAlign: TextAlign.center,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class ThirdPageArguments extends BaseArguments {
+  final String name;
+  final int price;
+
+  ThirdPageArguments({this.name, this.price});
+}
